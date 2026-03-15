996255ccad30b63cc3290f7b63c16f741f3178b8
Teletabi3 <szilvagyitamara@gmail.com>
2026-03-15 19:49:37 +0100

    int használat
    A program két egész számot bekér a felhasználótól, kiírja őket, majd kiszámolja és megjeleníti az összegüket, végül vár egy billentyűlenyomásra, mielőtt bezárulna.

---
+++ kiiratás_számolás.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int szam1; //Az int C#‑ban egy egész szám típus, amely tört vagy tizedesjegy nélküli számokat tud tárolni. 
+            int szam2;
+            int sum = 0;
+            Console.Write("Kérem az első számot:");
+            szam1 = Convert.ToInt32(Console.ReadLine()); //de lehetne az órán tanult a int.Parse() is
+            Console.WriteLine($"Az első szám értéke: {szam1}");
+            Console.Write("Kérem a második számot:");
+            szam2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"A második szám értéke: {szam2}");
+            sum = szam1 + szam2;
+            Console.WriteLine($"A két szám összege: {sum}");
+            Console.ReadKey();
+
+        }
+    }
+}
