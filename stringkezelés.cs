f4ab3b6cf480a7a2f8235282ea39742f049875e8
Teletabi3 <szilvagyitamara@gmail.com>
2026-03-15 19:22:07 +0100

    Monogram kiiratás
    A program bekéri a felhasználó nevét, majd a vezeték- és keresztnév első betűjéből monogramot készít és kiírja.

---
+++ stringkezelés.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string keresztnev = "";
+            string vezeteknev = "";
+            string monogram = "";
+            Console.Write("Kérem a Vezetéknevét:");
+            vezeteknev = Console.ReadLine();
+            Console.Write("Kérem a Keresztnevét:");
+            keresztnev = Console.ReadLine();
+
+            monogram = vezeteknev[0].ToString();
+            monogram += keresztnev[0].ToString();
+
+            Console.Write("A monogram:");
+            Console.WriteLine(monogram);
+            Console.ReadKey();
+        }
+    }
+}
