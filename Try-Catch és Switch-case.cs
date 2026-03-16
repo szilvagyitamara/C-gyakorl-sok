3245b53945777471906ebf54a7f2e3794bc59eab
Teletabi3 <szilvagyitamara@gmail.com>
2026-03-16 18:28:59 +0100

    Jegyek
    A program bekér egy jegyet a felhasználótól, majd a switch szerkezet segítségével kiírja, hogy milyen szöveges értékelés tartozik az adott osztályzathoz. Ha a felhasználó 1–5 közötti számot ad meg, akkor a megfelelő minősítés jelenik meg (pl. 5 → Jeles, 3 → Közepes). Ha nem ilyen számot ír be, akkor a „Nincs ilyen osztályzat!” üzenet jelenik meg. A végén a program egy billentyűlenyomásra vár, mielőtt bezárulna.

---
+++ Try-Catch és Switch-case.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Tanuló informatika jegye:");
+            int jegy = int.Parse(Console.ReadLine());
+            switch (jegy)
+            {
+                case 5:
+                    {
+                        Console.WriteLine("Jeles");
+                        break;
+                    }
+            
+                case 4:
+                    {
+                        Console.WriteLine("Jó");
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("Közepes");
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("Elégséges");
+                        break;
+                    }
+                case 1:
+                    {
+                        Console.WriteLine("Elégtelen");
+                        break;
+                    }
+                default:
+                    Console.WriteLine("Nincs ilyen osztályzat!");
+                    break;
+
+            }
+        Console.ReadKey();
+        
+        /*
+        try
+        {
+            Console.WriteLine("Kérek egy számot:");
+            int szam=int.Parse(Console.ReadLine());
+            Console.WriteLine("Kérek még egy számot:");
+            int szam2 = int.Parse(Console.ReadLine());
+        }
+        catch
+        {
+            Console.WriteLine("Há hülye vagy fönök!Ez nem szám!");
+        }*/
+
+    }
+    }
+}
